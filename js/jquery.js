425af5caa399e6421e06dc035bf9d6bfb101d708
Tracey Field <tnfield@hotmail.com>
2015-08-12 15:51:50 +0100

    Loads grid with correct sized blocks. Mouseover drawing in black.

--- js/jquery.js
+++ js/jquery.js
@@ -1,20 +1,27 @@
-var rowSize = 16;
+var rowSize = 100;
+var blockSize = $('#gridContainer').width()/rowSize;
 
 $(document).ready(function() {
-	$('p').css('color', 'red');
-
 	createGrid(rowSize);
+
+	black();
+
 
 });
 
 function createGrid(rowSize) {
-	for(var i = 0; i < rowSize; i++){
+	for(var i = 1; i <= rowSize; i++){
+		for(var j = 1; j <= rowSize; j++){
+			$('#gridContainer').append('<div class="gridBlock"></div>');
+		}
 		$('#gridContainer').append('<div class="row"></div>');
-		alert("i " + i);
 	}
-	for (var j = 0; j < rowSize; j++) {
-			$(".row").append('<div class="gridBlock"></div>');
-			alert("j " + j);
-	}	
+	$('.gridBlock').css('width',blockSize);
+	$('.gridBlock').css('height',blockSize);
 }
 
+function black() {
+	$('.gridBlock').mouseenter(function() {
+        $(this).css('background-color', 'black');
+    });
+}
