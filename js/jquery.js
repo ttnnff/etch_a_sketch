f9a085c687923f94c715525df88910f3c35f152d
Tracey Field <tnfield@hotmail.com>
2015-08-13 12:10:28 +0100

    Clear grid and create new. Changes to function strucure.

--- js/jquery.js
+++ js/jquery.js
@@ -1,27 +1,39 @@
-var rowSize = 100;
-var blockSize = $('#gridContainer').width()/rowSize;
+var gridSize = 64;
 
 $(document).ready(function() {
-	createGrid(rowSize);
-
-	black();
-
+	createGrid(gridSize);
 
 });
 
-function createGrid(rowSize) {
-	for(var i = 1; i <= rowSize; i++){
-		for(var j = 1; j <= rowSize; j++){
+
+function createGrid(gridSize) {
+	$('p').after('<div id="gridContainer"></div>'); 			//create container for blocks
+	var blockSize = $('#gridContainer').width()/gridSize - 2; 	//calculate size of blocks based on grid resolution
+
+	for(var i = 1; i <= gridSize; i++){ 						//create rows and blocks
+		for(var j = 1; j <= gridSize; j++){
 			$('#gridContainer').append('<div class="gridBlock"></div>');
 		}
 		$('#gridContainer').append('<div class="row"></div>');
 	}
-	$('.gridBlock').css('width',blockSize);
+
+	$('.gridBlock').css('width',blockSize);						//adjust size of blocks to fit grid resolution
 	$('.gridBlock').css('height',blockSize);
+
+	blockColor();													
 }
 
-function black() {
-	$('.gridBlock').mouseenter(function() {
-        $(this).css('background-color', 'black');
-    });
+function clearGrid() {											//remove grid and create a new one
+	alert("y");
+	$('#gridContainer').remove();
+	createGrid(gridSize);
 }
+
+function blockColor() {
+	$('.gridBlock').mouseenter(function() {
+		$(this).css('background-color', 'black');
+	});
+}
+
+
+
